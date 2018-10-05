bd2a3165737b2349890c8f010447f0f60d4adf6e
faraday58 <jarodriguezvera@gmail.com>
2018-10-05 13:21:50 -0500

    Geolocalizacion Grupo 5

--- EstacionamientoLocalizacionG52019-I/Form1.Designer.cs
+++ EstacionamientoLocalizacionG52019-I/Form1.Designer.cs
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gmcPosicionEstacionamiento = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // gmcPosicionEstacionamiento
+            // 
+            this.gmcPosicionEstacionamiento.Bearing = 0F;
+            this.gmcPosicionEstacionamiento.CanDragMap = true;
+            this.gmcPosicionEstacionamiento.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmcPosicionEstacionamiento.GrayScaleMode = false;
+            this.gmcPosicionEstacionamiento.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmcPosicionEstacionamiento.LevelsKeepInMemmory = 5;
+            this.gmcPosicionEstacionamiento.Location = new System.Drawing.Point(37, 35);
+            this.gmcPosicionEstacionamiento.MarkersEnabled = true;
+            this.gmcPosicionEstacionamiento.MaxZoom = 2;
+            this.gmcPosicionEstacionamiento.MinZoom = 2;
+            this.gmcPosicionEstacionamiento.MouseWheelZoomEnabled = true;
+            this.gmcPosicionEstacionamiento.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmcPosicionEstacionamiento.Name = "gmcPosicionEstacionamiento";
+            this.gmcPosicionEstacionamiento.NegativeMode = false;
+            this.gmcPosicionEstacionamiento.PolygonsEnabled = true;
+            this.gmcPosicionEstacionamiento.RetryLoadTile = 0;
+            this.gmcPosicionEstacionamiento.RoutesEnabled = true;
+            this.gmcPosicionEstacionamiento.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmcPosicionEstacionamiento.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmcPosicionEstacionamiento.ShowTileGridLines = false;
+            this.gmcPosicionEstacionamiento.Size = new System.Drawing.Size(421, 315);
+            this.gmcPosicionEstacionamiento.TabIndex = 0;
+            this.gmcPosicionEstacionamiento.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gmcPosicionEstacionamiento);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmcPosicionEstacionamiento;
     }
 }
 
--- EstacionamientoLocalizacionG52019-I/Form1.cs
+++ EstacionamientoLocalizacionG52019-I/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
 
 namespace EstacionamientoLocalizacionG52019_I
 {
     public partial class Form1 : Form
     {
+
+
+        double LatInicial = 19.3188;
+        double LongInicial = -99.1843;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+            gmcPosicionEstacionamiento.DragButton = MouseButtons.Left;
+            gmcPosicionEstacionamiento.CanDragMap = true;
+            gmcPosicionEstacionamiento.MapProvider = GMapProviders.GoogleMap;
+            gmcPosicionEstacionamiento.Position = new GMap.NET.PointLatLng(LatInicial, LongInicial);
+            gmcPosicionEstacionamiento.MinZoom = 0;
+            gmcPosicionEstacionamiento.MaxZoom = 24;
+            gmcPosicionEstacionamiento.Zoom = 9;
+            gmcPosicionEstacionamiento.AutoScroll = true;
         }
     }
 }
